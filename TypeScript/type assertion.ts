cb66b575e3cf1c2a57fe7a7f9422285d09e7555f
Ulan Sulaimanov <ulan.sulaimanov@latoken.com>
2022-02-16 02:49:47 +0600

    Remove fucking ts

--- TypeScript/type assertion.ts
+++
@@ -1,7 +0,0 @@
-/* <input id="myInp" type="text"> */
-// HTMLInputElement
-// HTMLInputElement.value
-
-const myInp: HTMLInputElement  = document.getElementById('myInp') as HTMLInputElement;
-
-myInp.value; // everthing is correct
--- TypeScript/type overloading.ts
+++
@@ -1,9 +0,0 @@
-// variable type overloading
-let userMedia: boolean | number;
-userMedia = false;
-userMedia = 12;
-
-// array type overloading
-let importantDates3: (Date | string)[];
-importantDates3.push(new Date());
-importantDates3.push('21:12:2021');
